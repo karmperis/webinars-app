39bcd7c82a64441f53eded40f7d3965fafac4f0d
Nikolaos Karmperis <karmperis@gmail.com>
2026-04-29 19:58:38 +0300

    organized project structure into backend and frontend

---
+++ backend/src/test/java/com/karmperis/webinars_app/WebinarsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karmperis.webinars_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebinarsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
