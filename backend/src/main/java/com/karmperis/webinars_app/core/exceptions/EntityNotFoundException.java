a499251b6328c6d2b216470518204903a80882c3
Nikolaos Karmperis <karmperis@gmail.com>
2026-05-09 14:03:33 +0300

    feat: add EntityNotFoundException for handling entity retrieval errors

---
+++ backend/src/main/java/com/karmperis/webinars_app/core/exceptions/EntityNotFoundException.java
@@ -0,0 +1,17 @@
+package com.karmperis.webinars_app.core.exceptions;
+
+/**
+ * Exception thrown when an expected entity cannot be found.
+ */
+public class EntityNotFoundException extends AppGenericException{
+    private static final String DEFAULT_CODE = "NotFound";
+
+    /**
+     * Create a new exception for a "not found" error.
+     * @param code    base application error code/prefix
+     * @param message readable error message
+     */
+    public EntityNotFoundException(String code, String message) {
+        super(code + DEFAULT_CODE, message);
+    }
+}
