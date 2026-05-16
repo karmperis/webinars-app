d9b4461207e27e36e6ebb3dd6546c8b884e24d3c
Nikolaos Karmperis <karmperis@gmail.com>
2026-05-16 19:09:54 +0300

    feat: enhance CapabilityInsertDTO with validation groups for improved validation sequence

--- backend/src/main/java/com/karmperis/webinarsapp/dto/CapabilityInsertDTO.java
+++ backend/src/main/java/com/karmperis/webinarsapp/dto/CapabilityInsertDTO.java
@@ -1,5 +1,6 @@
 package com.karmperis.webinarsapp.dto;
 
+import jakarta.validation.GroupSequence;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -8,12 +9,15 @@ import jakarta.validation.constraints.Size;
  * @param name capability name (required)
  * @param description optional capability description
  */
+@GroupSequence({CapabilityInsertDTO.First.class, CapabilityInsertDTO.Second.class, CapabilityInsertDTO.class})
 public record CapabilityInsertDTO(
-        @NotBlank(message = "The capability name cannot be blank.")
-        @Size(min = 4, max = 50, message = "The capability name must contain between 4 and 50 characters.")
+        @NotBlank(message = "The capability name cannot be blank.", groups = First.class)
+        @Size(min = 4, max = 50, message = "The capability name must contain between 4 and 50 characters.", groups = Second.class)
         String name,
 
         @Size(max = 255, message = "The capability description must contain maximum 255 characters.")
         String description
 ) {
+        public interface First {}
+        public interface Second {}
 }
