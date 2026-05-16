8dfa7dc52dbc8e13d911d983b3b3d4b71553d1c4
Nikolaos Karmperis <karmperis@gmail.com>
2026-05-16 23:11:43 +0300

    feat: add validation groups to RoleInsertDTO for improved validation sequence

--- backend/src/main/java/com/karmperis/webinarsapp/dto/RoleInsertDTO.java
+++ backend/src/main/java/com/karmperis/webinarsapp/dto/RoleInsertDTO.java
@@ -1,5 +1,6 @@
 package com.karmperis.webinarsapp.dto;
 
+import jakarta.validation.GroupSequence;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -7,9 +8,12 @@ import jakarta.validation.constraints.Size;
  * DTO used to create a new Role.
  * @param name the role name (must be non-blank and contain between 4 and 50 characters)
  */
+@GroupSequence({RoleInsertDTO.First.class, RoleInsertDTO.Second.class, RoleInsertDTO.class})
 public record RoleInsertDTO(
-        @NotBlank(message = "The role name cannot be blank.")
-        @Size(min = 4, max = 50, message = "The role name must contain between 4 and 50 characters.")
+        @NotBlank(message = "The role name cannot be blank.", groups = First.class)
+        @Size(min = 4, max = 50, message = "The role name must contain between 4 and 50 characters.", groups = Second.class)
         String name
 ) {
+        public interface First{}
+        public interface Second{}
 }
