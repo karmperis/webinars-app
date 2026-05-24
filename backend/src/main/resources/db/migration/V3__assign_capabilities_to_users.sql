55d0ecb1a53857ecf5146ebcfd97e12867e47414
Nikolaos Karmperis <karmperis@gmail.com>
2026-05-24 20:32:40 +0300

    feat: add SQL script to assign capabilities to user roles

---
+++ backend/src/main/resources/db/migration/V3__assign_capabilities_to_users.sql
@@ -0,0 +1,39 @@
+--V2__insert_roles_capabilities.sql
+--SQL Server 2022 (16.0.1175)
+--Collation: GREEK_CI_AS
+
+/*
+============================================================================
+Assign capabilities to ADMIN (all capabilities)
+============================================================================
+*/
+INSERT INTO roles_capabilities (role_id, capability_id)
+SELECT r.id, c.id
+FROM roles r
+         CROSS JOIN capabilities c
+WHERE r.name = 'ADMIN';
+
+/*
+============================================================================
+Assign capabilities to ORGANIZER (Specific capabilities)
+============================================================================
+*/
+
+INSERT INTO roles_capabilities (role_id, capability_id)
+SELECT r.id, c.id
+FROM roles r
+         CROSS JOIN capabilities c
+WHERE r.name = 'ORGANIZER'
+  AND c.name IN ('CREATE_WEBINAR', 'EDIT_WEBINAR', 'DELETE_WEBINAR', 'VIEW_WEBINARS');
+
+/*
+============================================================================
+Assign capabilities to PARTICIPANT (Specific capabilities)
+============================================================================
+*/
+INSERT INTO roles_capabilities (role_id, capability_id)
+SELECT r.id, c.id
+FROM roles r
+         CROSS JOIN capabilities c
+WHERE r.name = 'PARTICIPANT'
+  AND c.name IN ('ENROLL_IN_WEBINAR', 'VIEW_WEBINARS');
