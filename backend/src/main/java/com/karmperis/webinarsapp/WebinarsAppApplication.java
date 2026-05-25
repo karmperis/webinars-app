3d6b4483c9546c1e6acb41f23a65edbd02bac6b7
Nikolaos Karmperis <karmperis@gmail.com>
2026-05-25 17:24:56 +0300

    refactor: remove SecurityAutoConfiguration exclusion from SpringBootApplication

--- backend/src/main/java/com/karmperis/webinarsapp/WebinarsAppApplication.java
+++ backend/src/main/java/com/karmperis/webinarsapp/WebinarsAppApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//TODO: REMOVE SecurityAutoConfiguration.class
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 @EnableJpaAuditing
 public class WebinarsAppApplication {
 
