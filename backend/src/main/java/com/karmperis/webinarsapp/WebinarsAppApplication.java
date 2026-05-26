ee2dd78c5472777cbb2c558b7a36a695e69192b7
Nikolaos Karmperis <karmperis@gmail.com>
2026-05-27 01:11:53 +0300

    refactor: remove unused security auto-configuration import

--- backend/src/main/java/com/karmperis/webinarsapp/WebinarsAppApplication.java
+++ backend/src/main/java/com/karmperis/webinarsapp/WebinarsAppApplication.java
@@ -2,7 +2,6 @@ package com.karmperis.webinarsapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -12,5 +11,6 @@ public class WebinarsAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebinarsAppApplication.class, args);
     }
+
 
 }
