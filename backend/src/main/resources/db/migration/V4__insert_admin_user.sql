67f5aba35b6a122820d062a5a5dc90275fd6715f
Nikolaos Karmperis <karmperis@gmail.com>
2026-05-27 01:07:36 +0300

    feat: seed admin user and profile details in the database

---
+++ backend/src/main/resources/db/migration/V4__insert_admin_user.sql
@@ -0,0 +1,33 @@
+--V2__insert_roles_capabilities.sql
+--SQL Server 2022 (16.0.1175)
+--Collation: GREEK_CI_AS
+
+/*
+============================================================================
+Seed Admin User and Profile Details
+============================================================================
+*/
+IF NOT EXISTS
+    (SELECT 1
+     FROM users
+     WHERE username = 'admin')
+    BEGIN
+        /*
+        ============================================================================
+        Insert the default ADMIN account into users table
+        ============================================================================
+       */
+        INSERT INTO users (username, password, active, role_id, uuid)
+        VALUES ('admin', '$2a$10$dgxkyBEChDsvFm44UEHvZuZep27auxuWvdVjZrWPgewe9PY57obvy', 1, 1, NEWID());
+
+        DECLARE @AdminId INT;
+        SET @AdminId = SCOPE_IDENTITY();
+
+        /*
+        ============================================================================
+        Insert corresponding profile details into users_details table
+        ============================================================================
+        */
+        INSERT INTO users_details (user_id, firstname, lastname, phone_number)
+        VALUES (1, 'Nikos', 'Karmperis', '+306900000000');
+    END;
