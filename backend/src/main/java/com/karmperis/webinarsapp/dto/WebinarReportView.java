3f1c0bf13aff59840969e54183afe99475fbc057
Nikolaos Karmperis <karmperis@gmail.com>
2026-05-30 12:54:32 +0300

    feat: add WebinarReportView for aggregated reports

---
+++ backend/src/main/java/com/karmperis/webinarsapp/dto/WebinarReportView.java
@@ -0,0 +1,51 @@
+package com.karmperis.webinarsapp.dto;
+
+/**
+ * Projection view for aggregated webinar report fields.
+ */
+public interface WebinarReportView {
+    /**
+     * @return the webinar title
+     */
+    String getWebinarTitle();
+
+    /**
+     * @return the organizer username
+     */
+    String getOrganizerUsername();
+
+    /**
+     * @return the organizer first name
+     */
+    String getOrganizerFirstName();
+
+    /**
+     * @return the organizer last name
+     */
+    String getOrganizerLastName();
+
+    /**
+     * @return the total number of participants for the webinar
+     */
+    Integer getTotalParticipants();
+
+    /**
+     * @return the total number of webinars in the report scope
+     */
+    Integer getTotalWebinars();
+
+    /**
+     * @return the total duration of webinars in minutes
+     */
+    Integer getTotalDuration();
+
+    /**
+     * @return the webinar status label
+     */
+    String getWebinarStatus();
+
+    /**
+     * @return the user status label
+     */
+    String getUserStatus();
+}
