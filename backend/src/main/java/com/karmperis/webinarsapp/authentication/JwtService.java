e2cf6dec6975b54cefe44beb0d0c60153eaafa34
Nikolaos Karmperis <karmperis@gmail.com>
2026-05-31 18:45:04 +0300

    refactor: update JwtService to use fluent API for JWT creation and improve claims extraction

--- backend/src/main/java/com/karmperis/webinarsapp/authentication/JwtService.java
+++ backend/src/main/java/com/karmperis/webinarsapp/authentication/JwtService.java
@@ -2,13 +2,13 @@ package com.karmperis.webinarsapp.authentication;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
@@ -37,12 +37,12 @@ public class JwtService {
         claims.put("role", role);
         return Jwts
                 .builder()
-                .setIssuer("https://api.webinarsapp.com")
-                .setClaims(claims)
-                .setSubject(username)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
-                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .issuer("https://api.webinarsapp.com")
+                .claims(claims)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(getSignInKey())
                 .compact();
     }
 
@@ -119,12 +119,11 @@ public class JwtService {
      * @return the token claims
      */
     private Claims extractAllClaims(String token) {
-        return Jwts
-                .parserBuilder()
-                .setSigningKey(getSignInKey())
+        return Jwts.parser()
+                .verifyWith(getSignInKey())
                 .build()
-                .parseClaimsJws(token)
-                .getBody();
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
     /**
@@ -135,7 +134,7 @@ public class JwtService {
      *
      * @return a SecretKey which implements Key.
      */
-    private Key getSignInKey() {
+    private SecretKey getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
