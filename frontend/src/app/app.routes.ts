9ec659f886c795dd3715ab70ae6b7f15dd080117
KARMPERIS Nikolaos <karmperis@gmail.com>
2026-06-07 15:16:58 +0000

    feat: add webinars route with auth guard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
+
 import { Login } from './components/auth/login/login';
 import { Register } from './components/auth/register/register';
+import { Webinars } from './components/webinars/webinars/webinars';
+import { authGuard } from './shared/guards/auth-guard';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'register', component: Register },
+  { path: 'webinars', component: Webinars, canActivate: [authGuard] },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' },
 ];
