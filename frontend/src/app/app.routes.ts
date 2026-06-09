aa921d0a0daae88e9873293c505da7dea7ca8543
KARMPERIS Nikolaos <karmperis@gmail.com>
2026-06-09 13:27:48 +0000

    feat: add route for CreateWebinar component with auth guard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { Login } from './components/auth/login/login';
 import { Register } from './components/auth/register/register';
 import { Webinars } from './components/webinars/webinars/webinars';
 import { authGuard } from './shared/guards/auth-guard';
+import { CreateWebinar } from './components/webinars/create-webinar/create-webinar';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'register', component: Register },
   { path: 'webinars', component: Webinars, canActivate: [authGuard] },
+  { path: 'webinars/create', component: CreateWebinar, canActivate: [authGuard] },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' },
 ];
